959e5c5fd18872ec68096dd8b1da62ad70c4c8ac
Mikołaj Wielgos <43356643+wlgs@users.noreply.github.com>
2021-10-16 23:04:37 +0200

    Update lab1.sql

--- lab1.sql
+++ lab1.sql
@@ -1,31 +1,34 @@
 --Wybierz nazwy i adresy klientów, mających siedziby w Londynie
-
 SELECT ContactName, Address
 FROM Customers
 WHERE City='London';
---Wybierz nazwy i adresy klientów, mających siedziby w Londynie lub Madrycie
 
+--Wybierz nazwy i adresy klientów, mających siedziby w Londynie lub Madrycie
 SELECT ContactName, Address
 FROM Customers
 WHERE City='LONDON' or City='Madrid';
---Wybierz nazwy produktów, których cena jednostkowa jest większa niż 40
 
+--Wybierz nazwy produktów, których cena jednostkowa jest większa niż 40
 SELECT ProductName
 FROM Products
 WHERE UnitPrice>40;
+
 --Wybierz nazwy produktów, których cena jednostkowa jest większa niż 40, posortuj w porządku rosnącym
 SELECT ProductName
 FROM Products
 WHERE UnitPrice>40
 ORDER BY UnitPrice;
+
 --Podaj ile jest produktów, których cena jednostkowa jest większa niż 40
 SELECT COUNT(ProductID)
 FROM Products
 WHERE UnitPrice>40;
+
 --Podaj ile jest produktów, których cena jednostkowa jest większa niż 40, których stan magazynowy jest powyżej 100 sztuk
 SELECT COUNT(ProductID)
 FROM Products
 WHERE UnitPrice>40 and UnitsInStock>100;
+
 --Podaj ile jest produktów z kategorii 2-3, których cena jednostkowa jest większa niż 40, których stan magazynowy jest powyżej 100 sztuk
 SELECT COUNT(ProductID)
 FROM Products
